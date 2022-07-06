b01958df8931400a8d48f72b163e898dd591afc3
Yuichi Yoshida <yoshida.yuichi@gmail.com>
2022-07-06 17:37:41 +0900

    Fixed Package.swift.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,29 +6,26 @@ import PackageDescription
 let package = Package(
     name: "Matft",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Matft",
             targets: ["Matft"]),
-        //.library(name: "ReleaseTest", targets: ["Matft "])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-collections", from: "0.0.1"),
-
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Matft",
-            dependencies: ["Collections"]),
+            dependencies: [
+                .product(name: "Collections", package: "swift-collections")
+            ]),
         .testTarget(
             name: "MatftTests",
-            dependencies: ["Matft"]),
-        .testTarget(
-            name: "PerformanceTests",
             dependencies: ["Matft"]),
     ]
 )
